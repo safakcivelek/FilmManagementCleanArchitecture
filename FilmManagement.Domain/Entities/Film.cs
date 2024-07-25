aa0e5f4424935a61e5048647d64fcbd4759acaf3
safakcivelek <safakcivelek3@gmail.com>
2024-07-25 21:43:14 +0300

    Update

--- FilmManagement.Domain/Entities/Film.cs
+++ FilmManagement.Domain/Entities/Film.cs
@@ -9,9 +9,9 @@
 
         public Guid DirectorId { get; set; }
         
-        public Director Director { get; set; }
-        public ICollection<FilmGenre> FilmGenres { get; set; }
-        public ICollection<FilmActor> FilmActors { get; set; }
-        public ICollection<Purchase> Purchases { get; set; }
+        public Director? Director { get; set; }
+        public ICollection<FilmGenre?> FilmGenres { get; set; }
+        public ICollection<FilmActor?> FilmActors { get; set; }
+        public ICollection<Purchase?> Purchases { get; set; }
     }
 }
