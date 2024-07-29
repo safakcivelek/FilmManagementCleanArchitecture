4a42e1bc808304e7cc543f2b62ca32a8a75c68ef
safakcivelek <safakcivelek3@gmail.com>
2024-07-29 20:13:29 +0300

    Refactor: Use FilmServiceMessages for centralized message management in FilmService

--- FilmManagement.Application/Concretes/Services/FilmService.cs
+++ FilmManagement.Application/Concretes/Services/FilmService.cs
@@ -1,6 +1,7 @@
 ﻿using FilmManagement.Application.Abstracts.Repositories;
 using FilmManagement.Application.Abstracts.Services;
 using FilmManagement.Application.Common.Responses;
+using FilmManagement.Application.Features.Films.Constants;
 using FilmManagement.Domain.Entities;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
@@ -20,16 +21,16 @@ namespace FilmManagement.Application.Concretes.Services
         {
             Film? film = await _filmRepository.GetAsync(predicate, include, enableTracking, withDeleted);
             if (film == null)
-                return new ApiResponse<Film>(film, "Film bulunamadı.",404);
-            return new ApiResponse<Film>(film, "Film başarıyla getirildi.");
+                return new ApiResponse<Film>(film,FilmServiceMessages.FilmNotFound,404);
+            return new ApiResponse<Film>(film,FilmServiceMessages.FilmRetrievedSuccessfully);
         }
 
         public async Task<ApiListResponse<Film>> GetListAsync(Expression<Func<Film, bool>>? predicate = null, Func<IQueryable<Film>, IIncludableQueryable<Film, object>>? include = null, bool enableTracking = true, bool withDeleted = false)
         {
             IList<Film> filmList = await _filmRepository.GetListAsync(predicate, include, enableTracking, withDeleted);
             if (filmList.Count == 0)
-                return new ApiListResponse<Film>(filmList, "Hiç film bulunamadı.", 404);
-            return new ApiListResponse<Film>(filmList, "Filmler başarıyla listelendi.");
+                return new ApiListResponse<Film>(filmList, FilmServiceMessages.NoFilmsFound, 404);
+            return new ApiListResponse<Film>(filmList, FilmServiceMessages.FilmsListedSuccessfully);
         }
 
         public async Task<bool> AnyAsync(Expression<Func<Film, bool>>? predicate = null, bool enableTracking = true, bool withDeleted = false)
@@ -41,36 +42,36 @@ namespace FilmManagement.Application.Concretes.Services
         public async Task<ApiResponse<Film>> AddAsync(Film film)
         {
             Film addedFilm = await _filmRepository.AddAsync(film);
-            return new ApiResponse<Film>(addedFilm, "Film başarıyla oluşturuldu.");
+            return new ApiResponse<Film>(addedFilm, FilmServiceMessages.FilmAddedSuccessfully);
         }
 
         public async Task<ApiResponse<Film>> UpdateAsync(Film film)
         {
             Film updatedFilm = await _filmRepository.UpdateAsync(film);
-            return new ApiResponse<Film>(updatedFilm, "Film başarıyla güncellendi.");
+            return new ApiResponse<Film>(updatedFilm, FilmServiceMessages.FilmUpdatedSuccessfully);
         }
         public async Task<ApiResponse<Film>> DeleteAsync(Film film)
         {
             Film deletedFilm = await _filmRepository.DeleteAsync(film);
-            return new ApiResponse<Film>(deletedFilm, "Film başarıyla silindi.");
+            return new ApiResponse<Film>(deletedFilm, FilmServiceMessages.FilmDeletedSuccessfully);
         }
 
         public async Task<ApiListResponse<Film>> AddRangeAsync(IList<Film> films)
         {
             IList<Film> addedFilms = await _filmRepository.AddRangeAsync(films);
-            return new ApiListResponse<Film>(addedFilms, "Filmler başarıyla eklendi.");
+            return new ApiListResponse<Film>(addedFilms, FilmServiceMessages.FilmsAddedSuccessfully);
         }
 
         public async Task<ApiListResponse<Film>> UpdateRangeAsync(IList<Film> films)
         {
             IList<Film> updatedFilms = await _filmRepository.UpdateRangeAsync(films);
-            return new ApiListResponse<Film>(updatedFilms, "Filmler başarıyla güncellendi.");
+            return new ApiListResponse<Film>(updatedFilms, FilmServiceMessages.FilmsUpdatedSuccessfully);
         }
 
         public async Task<ApiListResponse<Film>> DeleteRangeAsync(IList<Film> films)
         {
             IList<Film> deletedFilms = await _filmRepository.DeleteRangeAsync(films);
-            return new ApiListResponse<Film>(deletedFilms, "Filmler başarıyla silindi.");
+            return new ApiListResponse<Film>(deletedFilms, FilmServiceMessages.FilmsDeletedSuccessfully);
         }
     }
 }
---
+++ FilmManagement.Application/Features/Films/Constants/FilmServiceMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace FilmManagement.Application.Features.Films.Constants
+{
+    public static class FilmServiceMessages
+    {
+        public const string FilmRetrievedSuccessfully = "Film başarıyla getirildi.";
+        public const string FilmNotFound = "Film bulunamadı.";
+        public const string NoFilmsFound = "Hiç film bulunamadı.";
+        public const string FilmsListedSuccessfully = "Filmler başarıyla listelendi.";
+        public const string FilmAddedSuccessfully = "Film başarıyla eklendi.";
+        public const string FilmUpdatedSuccessfully = "Film başarıyla güncellendi.";
+        public const string FilmDeletedSuccessfully = "Film başarıyla silindi.";
+        public const string FilmsAddedSuccessfully = "Filmler başarıyla eklendi.";
+        public const string FilmsUpdatedSuccessfully = "Filmler başarıyla güncellendi.";
+        public const string FilmsDeletedSuccessfully = "Filmler başarıyla silindi.";
+    }
+}
