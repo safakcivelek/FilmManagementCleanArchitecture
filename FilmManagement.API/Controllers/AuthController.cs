a84f66e793392be72e345162916bde179a7ed473
safakcivelek <safakcivelek3@gmail.com>
2024-08-13 04:09:01 +0300

    Add AuthController for handling user registration

---
+++ FilmManagement.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using FilmManagement.Application.Common.Responses;
+using FilmManagement.Application.Features.Auth.Commands.Register;
+using FilmManagement.Application.Features.Auth.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        //[Authorize(Roles ="user")]
+        //[Authorize]
+        public async Task<IActionResult> Register(RegisterCommandRequest request)
+        {
+            ApiResponse<RegisterResponseDto> response = await _mediator.Send(request);
+            return Created(uri: "", response);
+        }
+    }
+}
--- FilmManagement.API/Program.cs
+++ FilmManagement.API/Program.cs
@@ -45,6 +45,7 @@ app.ConfigureCustomExceptionMiddleware();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
