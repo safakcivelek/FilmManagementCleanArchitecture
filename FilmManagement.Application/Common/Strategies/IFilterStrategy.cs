ba0cb3aae2fc26ba029231a9f7a41d82ec151cd7
safakcivelek <safakcivelek3@gmail.com>
2024-08-21 17:46:54 +0300

    Removed IFilterStrategy and FilmFilterStrategy classes

--- FilmManagement.Application/Common/Strategies/IFilterStrategy.cs
+++
@@ -1,9 +0,0 @@
-﻿using FilmManagement.Application.Common.Dynamic;
-
-namespace FilmManagement.Application.Common.Strategies
-{
-    public interface IFilterStrategy<TEntity>
-    {
-        IQueryable<TEntity> ApplyFilter(IQueryable<TEntity> query, DynamicQuery dynamicQuery);
-    }
-}
