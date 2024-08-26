65b2e3a76d5630c67499930b3b9b16468f95edae
safakcivelek <safakcivelek3@gmail.com>
2024-08-26 23:49:22 +0300

    Added GenreServiceMessages class to manage constant service messages for genre-related operations.

---
+++ FilmManagement.Application/Features/Genres/Constants/GenreServiceMessages.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FilmManagement.Application.Features.Genres.Constants
+{
+    public class GenreServiceMessages
+    {
+        public const string GenreRetrievedSuccessfully = "Film türü başarıyla getirildi.";
+        public const string GenreNotFound = "Film türü bulunamadı.";
+        public const string NoGenresFound = "Hiç Film türü bulunamadı.";
+        public const string GenresListedSuccessfully = "Film türleri başarıyla listelendi.";
+        public const string GenreAddedSuccessfully = "Film türü başarıyla eklendi.";
+        public const string GenreUpdatedSuccessfully = "Film türü başarıyla güncellendi.";
+        public const string GenreDeletedSuccessfully = "Film türü başarıyla silindi.";
+        public const string GenresAddedSuccessfully = "Film türleri başarıyla eklendi.";
+        public const string GenresUpdatedSuccessfully = "Film türleri başarıyla güncellendi.";
+        public const string GenresDeletedSuccessfully = "Film türleri başarıyla silindi.";
+    }
+}
