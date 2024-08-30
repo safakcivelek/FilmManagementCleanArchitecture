8dc6ecf1c03eb8cfd32a16ad850b659daaf7cfb1
safakcivelek <safakcivelek3@gmail.com>
2024-08-30 13:23:30 +0300

    fix: Corrected the property name from 'FimId' to 'FilmId' in the Purchase entity.

--- FilmManagement.Domain/Entities/Purchase.cs
+++ FilmManagement.Domain/Entities/Purchase.cs
@@ -3,7 +3,7 @@
     public class Purchase : BaseEntity<Guid>
     {
         public Guid UserId { get; set; }
-        public Guid FimId { get; set; }
+        public Guid FilmId { get; set; }
 
         public decimal Price { get; set; }      
 
