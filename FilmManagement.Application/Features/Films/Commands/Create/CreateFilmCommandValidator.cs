51be9a202fe5c2587cb14f1abe37005fb8b17fcb
safakcivelek <safakcivelek3@gmail.com>
2024-09-23 02:21:08 +0300

    Update validation rules for duration, image, video, and year fields in CreateFilmCommandValidator

--- FilmManagement.Application/Features/Films/Commands/Create/CreateFilmCommandValidator.cs
+++ FilmManagement.Application/Features/Films/Commands/Create/CreateFilmCommandValidator.cs
@@ -11,7 +11,7 @@ namespace FilmManagement.Application.Features.Films.Commands.Create
                 .MaximumLength(100).WithMessage("Film adı en fazla 100 karakter olabilir.");
 
             RuleFor(f => f.Year)
-                .InclusiveBetween(1900, DateTime.Now.Year).WithMessage($"Film yılı {DateTime.Now.Year} ve 1900 arasında olmalıdır.");
+                .NotEmpty().InclusiveBetween(1900, DateTime.Now.Year).WithMessage($"Film yılı {DateTime.Now.Year} ve 1900 arasında olmalıdır.");
 
             RuleFor(f => f.Price)
                 .GreaterThan(0).WithMessage("Fiyat 0'dan büyük olmalıdır.");
@@ -24,11 +24,23 @@ namespace FilmManagement.Application.Features.Films.Commands.Create
 
             RuleFor(f => f.GenreIds)
                 .Must(g => g != null && g.Any()).WithMessage("En az bir film türü seçilmelidir.") // Null olamaz!
-                .ForEach(g => g.NotEmpty().WithMessage("Oyuncu ID boş olamaz.")); // örneğin, boş bir string ("") veya sıfır değeri gibi olamaz!
+                .ForEach(g => g.NotEmpty().WithMessage("Tür ID boş olamaz.")); // örneğin, boş bir string ("") veya sıfır değeri gibi olamaz!
 
             RuleFor(x => x.ActorIds)
                 .Must(a => a != null && a.Any()).WithMessage("En az bir oyuncu seçilmelidir.")
-                .ForEach(a => a.NotEmpty().WithMessage("Oyuncu ID boş olamaz."));          
+                .ForEach(a => a.NotEmpty().WithMessage("Oyuncu ID boş olamaz."));
+
+            RuleFor(f => f.Duration)
+            .NotEmpty().GreaterThan(0).WithMessage("Süre 0'dan büyük olmalıdır.")
+            .LessThanOrEqualTo(600).WithMessage("Süre en fazla 600 dakika olabilir.");
+
+            RuleFor(f => f.Image)
+                .Must(i => string.IsNullOrEmpty(i) || Uri.IsWellFormedUriString(i, UriKind.Absolute))
+                .WithMessage("Geçerli bir URL formatı sağlanmalıdır."); // Zorunlu değil ama varsa geçerli URL olmalı
+
+            RuleFor(f => f.Video)
+                .Must(v => string.IsNullOrEmpty(v) || Uri.IsWellFormedUriString(v, UriKind.Absolute))
+                .WithMessage("Geçerli bir URL formatı sağlanmalıdır.");
         }
     }
 }
