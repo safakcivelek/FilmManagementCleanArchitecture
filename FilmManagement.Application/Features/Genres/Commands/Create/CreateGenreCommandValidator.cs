602966399ee7dd1c1164deadd1ed73666efd4114
safakcivelek <safakcivelek3@gmail.com>
2024-09-25 04:03:59 +0300

    Fix character limit in description field

--- FilmManagement.Application/Features/Genres/Commands/Create/CreateGenreCommandValidator.cs
+++ FilmManagement.Application/Features/Genres/Commands/Create/CreateGenreCommandValidator.cs
@@ -12,7 +12,7 @@ namespace FilmManagement.Application.Features.Genres.Commands.Create
                 .MaximumLength(50).WithMessage("Tür adı en fazla 50 karakter olabilir.");
 
             RuleFor(g => g.Description)
-                .MaximumLength(50).WithMessage("Açıklama alanı en fazla 500 karakter olabilir.");
+                .MaximumLength(500).WithMessage("Açıklama alanı en fazla 500 karakter olabilir.");
         }
     }
 }
